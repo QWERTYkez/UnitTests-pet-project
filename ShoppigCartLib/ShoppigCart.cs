60cba6eca7c6a2837e94751a18881a1a9bb2d836
Fantom TM <QWERTYkez@outlook.com>
2019-10-03 22:25:59 +0700

    ShoppigCartTests

---
+++ ShoppigCartLib/ShoppigCart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppigCartLib
+{
+    public class ShoppigCart
+    {
+        public List<Item> Items = new List<Item>();
+
+        public int Count
+        {
+            get { return Items.Count; }
+        }
+
+        public void Add(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void Remove(int index)
+        {
+            Items.RemoveAt(index);
+        }
+
+        public void Dispose()
+        {
+            Items = null;
+        }
+    }
+
+    public class Item
+    {
+        public Item(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        public string Name;
+        public int Quantity;
+    }
+}
---
+++ ShoppigCartLibTests/ShoppigCartTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppigCartLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppigCartLib.Tests
+{
+    [TestClass()]
+    public class ShoppigCartTests
+    {
+        private ShoppigCart cart;
+        private Item item;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Debug.WriteLine("TestInitialize");
+            item = new Item("TestName", 10);
+
+            cart = new ShoppigCart();
+            cart.Add(item);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Debug.WriteLine("TestCleanup");
+            cart.Dispose();
+        }
+
+        [TestMethod()]
+        public void Cart_Check_ContainsItem()
+        {
+            CollectionAssert.Contains(cart.Items, item);
+        }
+
+        [TestMethod()]
+        public void Cart_Remove_Empty()
+        {
+            cart.Remove(0);
+            Assert.AreEqual(0, cart.Count);
+        }
+    }
+}
